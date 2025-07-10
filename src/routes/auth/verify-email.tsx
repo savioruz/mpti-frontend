590f389dff52be7367acfcc4ed69196bafbf7314
savioruz <kheril.dev@gmail.com>
2025-07-10 15:42:26 +0700

    feat: implement email verification route and update component structure

--- src/routes/auth/verify-email.tsx
+++ src/routes/auth/verify-email.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { EmailVerificationForm } from "@/components/pages/email-verification-form";
 
-export const Route = createFileRoute('/auth/verify-email')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/auth/verify-email")({
+  component: EmailVerification,
+});
 
-function RouteComponent() {
-  return <div>Hello "/auth/verify-email"!</div>
+function EmailVerification() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <EmailVerificationForm />
+      </div>
+    </div>
+  );
 }
